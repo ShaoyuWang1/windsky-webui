0293b491d60512ff0e6d4fa527faa131d24585e8
windsky wang <40138256+ShaoyuWang1@users.noreply.github.com>
2025-02-20 19:18:07 -0500

    修改完善改名功能

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -478,8 +478,6 @@
 		if (backendConfig) {
 			// Save Backend Status to Store
 			await config.set(backendConfig);
-			await WEBUI_NAME.set(backendConfig.name);
-
 			if ($config) {
 				await setupSocket($config.features?.enable_websocket ?? true);
 
